c9b680de5aa3032cd6e03f15e230026f0c46d78b
Wellington Moreno <jwellington.moreno@gmail.com>
2016-12-13 08:19:48 -0800

    Enhances Test Generators

--- src/test/java/tech/redroma/google/places/data/Generators.java
+++ src/test/java/tech/redroma/google/places/data/Generators.java
@@ -16,15 +16,20 @@
 
 package tech.redroma.google.places.data;
 
+import tech.redroma.google.places.requests.AutocompletePlaceRequest;
+import tech.redroma.google.places.requests.GetPhotoRequest;
+import tech.redroma.google.places.requests.GetPlaceDetailsRequest;
 import tech.redroma.google.places.requests.NearbySearchRequest;
 import tech.sirwellington.alchemy.generator.AlchemyGenerator;
 
 import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.CollectionGenerators.listOf;
 import static tech.sirwellington.alchemy.generator.EnumGenerators.enumValueOf;
 import static tech.sirwellington.alchemy.generator.GeolocationGenerators.latitudes;
 import static tech.sirwellington.alchemy.generator.GeolocationGenerators.longitudes;
 import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
 import static tech.sirwellington.alchemy.generator.StringGenerators.alphabeticString;
+import static tech.sirwellington.alchemy.generator.StringGenerators.hexadecimalString;
 
 /**
  *
@@ -92,5 +97,34 @@ public class Generators
     private static Language oneLanguage()
     {
         return enumValueOf(Language.class).get();
+    }
+    
+    public static AutocompletePlaceRequest createAutocompleteRequest()
+    {
+        return AutocompletePlaceRequest.newBuilder()
+            .withInput(one(alphabeticString()))
+            .withLocation(one(locations()))
+            .withRadiusInMeters(createRadius())
+            .withTypes(listOf(enumValueOf(Types.AutocompleteType.class)))
+            .build();
+    }
+    
+    public static GetPlaceDetailsRequest createGetPlaceDetailsRequest()
+    {
+        return GetPlaceDetailsRequest.newBuilder()
+            .withExtensions(enumValueOf(Extensions.class).get())
+            .withLanguage(oneLanguage())
+            .withPlaceID(one(hexadecimalString(12)))
+            .build();
+    }
+    
+    public static GetPhotoRequest createGetPhotoRequest()
+    {
+        AlchemyGenerator<Integer> widths = integers(GetPhotoRequest.Builder.MIN_WIDTH, GetPhotoRequest.Builder.MAX_WIDTH +1);
+        
+        return GetPhotoRequest.newBuilder()
+            .withPhotoReference(one(hexadecimalString(20)))
+            .withMaxHeight(one(widths))
+            .build();
     }
 }
