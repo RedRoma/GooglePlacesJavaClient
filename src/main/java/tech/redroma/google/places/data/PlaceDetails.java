999a82f6135b2ebdfc04fe2fc74edb00de9aa95d
Wellington Moreno <jwellington.moreno@gmail.com>
2016-12-14 00:07:43 -0800

    Expands PlaceDetails

--- src/main/java/tech/redroma/google/places/data/PlaceDetails.java
+++ src/main/java/tech/redroma/google/places/data/PlaceDetails.java
@@ -18,6 +18,8 @@ package tech.redroma.google.places.data;
 
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
+import java.util.Objects;
+import sir.wellington.alchemy.collections.lists.Lists;
 import tech.sirwellington.alchemy.annotations.concurrency.Mutable;
 import tech.sirwellington.alchemy.annotations.concurrency.ThreadUnsafe;
 import tech.sirwellington.alchemy.annotations.objects.Pojo;
@@ -72,15 +74,223 @@ public final class PlaceDetails
 
     String website;
 
+    @Override
+    public int hashCode()
+    {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.addressComponents);
+        hash = 97 * hash + Objects.hashCode(this.formattedAddress);
+        hash = 97 * hash + Objects.hashCode(this.formattedPhoneNumber);
+        hash = 97 * hash + Objects.hashCode(this.internationalPhoneNumber);
+        hash = 97 * hash + Objects.hashCode(this.geometry);
+        hash = 97 * hash + Objects.hashCode(this.iconURL);
+        hash = 97 * hash + Objects.hashCode(this.id);
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.openingHours);
+        hash = 97 * hash + Objects.hashCode(this.photos);
+        hash = 97 * hash + Objects.hashCode(this.placeId);
+        hash = 97 * hash + Objects.hashCode(this.rating);
+        hash = 97 * hash + Objects.hashCode(this.reference);
+        hash = 97 * hash + Objects.hashCode(this.reviews);
+        hash = 97 * hash + Objects.hashCode(this.types);
+        hash = 97 * hash + Objects.hashCode(this.url);
+        hash = 97 * hash + Objects.hashCode(this.utcOffset);
+        hash = 97 * hash + Objects.hashCode(this.vicinity);
+        hash = 97 * hash + Objects.hashCode(this.website);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final PlaceDetails other = (PlaceDetails) obj;
+        if (!Objects.equals(this.formattedAddress, other.formattedAddress))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.formattedPhoneNumber, other.formattedPhoneNumber))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.internationalPhoneNumber, other.internationalPhoneNumber))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.iconURL, other.iconURL))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.placeId, other.placeId))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.reference, other.reference))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.url, other.url))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.vicinity, other.vicinity))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.website, other.website))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.addressComponents, other.addressComponents))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.geometry, other.geometry))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.openingHours, other.openingHours))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.photos, other.photos))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.rating, other.rating))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.reviews, other.reviews))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.types, other.types))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.utcOffset, other.utcOffset))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "PlaceDetails{" + "addressComponents=" + addressComponents + ", formattedAddress=" + formattedAddress + ", formattedPhoneNumber=" + formattedPhoneNumber + ", internationalPhoneNumber=" + internationalPhoneNumber + ", geometry=" + geometry + ", iconURL=" + iconURL + ", id=" + id + ", name=" + name + ", openingHours=" + openingHours + ", photos=" + photos + ", placeId=" + placeId + ", rating=" + rating + ", reference=" + reference + ", reviews=" + reviews + ", types=" + types + ", url=" + url + ", utcOffset=" + utcOffset + ", vicinity=" + vicinity + ", website=" + website + '}';
+    }
+
+    /**
+     * This represents a piece of an Address.
+     */
     @Pojo
     @Mutable
     @ThreadUnsafe
-    public static class AddressComponent
+    public final static class AddressComponent
     {
 
+        /**
+         * The full text description or name of the address component.
+         */
         String longName;
+        /**
+         * The abbreviated textual name for the address component, if available. For example, an address component for the state
+         * of Alaska may have a 'long_name' or Alaska, but a 'short_name' of 'AK', using the 2-letter postal abbreviation.
+         */
         String shortName;
+
+        /**
+         * An array indicating the type of the address component.
+         */
         List<Types.ReturnedPlaceType> types;
+
+        public AddressComponent()
+        {
+        }
+
+        public String getLongName()
+        {
+            return longName;
+        }
+
+        public String getShortName()
+        {
+            return shortName;
+        }
+
+        public List<Types.ReturnedPlaceType> getTypes()
+        {
+            return Lists.immutableCopyOf(types);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            int hash = 7;
+            hash = 53 * hash + Objects.hashCode(this.longName);
+            hash = 53 * hash + Objects.hashCode(this.shortName);
+            hash = 53 * hash + Objects.hashCode(this.types);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+            if (obj == null)
+            {
+                return false;
+            }
+            if (getClass() != obj.getClass())
+            {
+                return false;
+            }
+            final AddressComponent other = (AddressComponent) obj;
+            if (!Objects.equals(this.longName, other.longName))
+            {
+                return false;
+            }
+            if (!Objects.equals(this.shortName, other.shortName))
+            {
+                return false;
+            }
+            if (!Objects.equals(this.types, other.types))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "AddressComponent{" + "longName=" + longName + ", shortName=" + shortName + ", types=" + types + '}';
+        }
+
     }
 
 }
